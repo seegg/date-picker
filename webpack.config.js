292de86485ee354a7b72ae6b886bb0d3ed4736b4
yuan tan <dyxtan@gmail.com>
2022-01-28 00:50:51 +1300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+
+module.exports = {
+  entry: ['./src/datepicker.ts'],
+  output: {
+    path: path.join(__dirname, 'scripts'),
+    filename: 'datepicker.min.js'
+  },
+  watch: true,
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(j|t)s$/i,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      }
+    ]
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.json'],
+  },
+  devtool: 'source-map',
+}
